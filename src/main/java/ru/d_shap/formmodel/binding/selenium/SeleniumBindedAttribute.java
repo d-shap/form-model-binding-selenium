7aed60ba4db19598d9ceec4b0299243770f5f71b
Dmitry Shapovalov <shenf@mail.ru>
2018-10-28 14:09:53 +0300

    Реализация для атрибута selenium

---
+++ src/main/java/ru/d_shap/formmodel/binding/selenium/SeleniumBindedAttribute.java
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model selenium binding is a form model binding implementation for Selenium.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model selenium binding.
+//
+// Form model selenium binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model selenium binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.selenium;
+
+import ru.d_shap.formmodel.binding.html.HtmlBindedAttribute;
+
+/**
+ * The selenium binded attribute.
+ *
+ * @author Dmitry Shapovalov
+ */
+public interface SeleniumBindedAttribute extends HtmlBindedAttribute {
+
+}
---
+++ src/main/java/ru/d_shap/formmodel/binding/selenium/SeleniumBindedAttributeImpl.java
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model selenium binding is a form model binding implementation for Selenium.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model selenium binding.
+//
+// Form model selenium binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model selenium binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.selenium;
+
+import ru.d_shap.formmodel.binding.html.HtmlBindedAttribute;
+
+/**
+ * The selenium binded attribute implementation.
+ *
+ * @author Dmitry Shapovalov
+ */
+final class SeleniumBindedAttributeImpl implements SeleniumBindedAttribute {
+
+    private final HtmlBindedAttribute _htmlBindedAttribute;
+
+    SeleniumBindedAttributeImpl(final HtmlBindedAttribute htmlBindedAttribute) {
+        super();
+        _htmlBindedAttribute = htmlBindedAttribute;
+    }
+
+    @Override
+    public String getValue() {
+        return _htmlBindedAttribute.getValue();
+    }
+
+    @Override
+    public String getAbsoluteValue() {
+        return _htmlBindedAttribute.getAbsoluteValue();
+    }
+
+}
