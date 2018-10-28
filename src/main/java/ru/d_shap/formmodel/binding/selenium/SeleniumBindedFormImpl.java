bb5b59144d710c38ba360fa65aa2d57a48b536a9
Dmitry Shapovalov <shenf@mail.ru>
2018-10-28 14:29:28 +0300

    Реализация для формы selenium

---
+++ src/main/java/ru/d_shap/formmodel/binding/selenium/SeleniumBindedFormImpl.java
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model selenium binding is a form model binding implementation for Selenium.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model selenium binding.
+//
+// Form model selenium binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model selenium binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.selenium;
+
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+
+import ru.d_shap.formmodel.binding.html.HtmlBindedForm;
+
+/**
+ * The Selenium binded form implementation.
+ *
+ * @author Dmitry Shapovalov
+ */
+final class SeleniumBindedFormImpl implements SeleniumBindedForm {
+
+    private final WebDriver _webDriver;
+
+    private final HtmlBindedForm _htmlBindedForm;
+
+    SeleniumBindedFormImpl(final WebDriver webDriver, final HtmlBindedForm htmlBindedForm) {
+        super();
+        _webDriver = webDriver;
+        _htmlBindedForm = htmlBindedForm;
+    }
+
+    @Override
+    public Document getDocument() {
+        return _htmlBindedForm.getDocument();
+    }
+
+    @Override
+    public WebDriver getWebDriver() {
+        return _webDriver;
+    }
+
+}
