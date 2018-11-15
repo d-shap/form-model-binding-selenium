ac5dfa319db9676bcfe4225ce61c7f790bb09c66
Dmitry Shapovalov <shenf@mail.ru>
2018-11-15 14:16:52 +0300

    Закомментировал код

--- src/main/java/ru/d_shap/formmodel/binding/selenium/SeleniumFormBinder.java
+++ src/main/java/ru/d_shap/formmodel/binding/selenium/SeleniumFormBinder.java
@@ -19,141 +19,74 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.formmodel.binding.selenium;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import ru.d_shap.formmodel.binding.FormBinder;
-import ru.d_shap.formmodel.binding.FormBindingException;
-import ru.d_shap.formmodel.definition.ElementDefinition;
-import ru.d_shap.formmodel.definition.FormDefinition;
-import ru.d_shap.formmodel.definition.FormDefinitions;
-import ru.d_shap.formmodel.definition.FormReferenceDefinition;
-
 /**
  * Selenium form binder.
  *
  * @author Dmitry Shapovalov
  */
-public final class SeleniumFormBinder extends FormBinder<SeleniumForm, SeleniumElement, SeleniumFormReference, Element> {
-
-    private final WebDriver _webDriver;
-
-    private Document _document;
+public final class SeleniumFormBinder {
 
     /**
      * Create new object.
-     *
-     * @param formDefinitions form definitions.
-     * @param webDriver       web driver.
      */
-    public SeleniumFormBinder(final FormDefinitions formDefinitions, final WebDriver webDriver) {
-        super(formDefinitions);
-        _webDriver = webDriver;
+    public SeleniumFormBinder() {
+        super();
     }
 
-    @Override
-    protected SeleniumForm createBindedFormInstance(final FormDefinition formDefinition) {
-        createDom(formDefinition);
-        return new SeleniumForm(formDefinition);
-    }
+    //    private void createDom(final FormDefinition formDefinition) {
+    //        _webDriver.switchTo().defaultContent();
+    //        String frame = formDefinition.getAdditionalAttribute("frame");
+    //        if (frame != null) {
+    //            String[] frameParts = frame.split("\\.");
+    //            for (String framePart : frameParts) {
+    //                try {
+    //                    int frameIndex = Integer.parseInt(framePart);
+    //                    _webDriver.switchTo().frame(frameIndex);
+    //                } catch (NumberFormatException ex) {
+    //                    _webDriver.switchTo().frame(framePart);
+    //                }
+    //            }
+    //        }
+    //        String html = _webDriver.getPageSource();
+    //        _document = Jsoup.parse(html);
+    //    }
 
-    private void createDom(final FormDefinition formDefinition) {
-        _webDriver.switchTo().defaultContent();
-        String frame = formDefinition.getAdditionalAttribute("frame");
-        if (frame != null) {
-            String[] frameParts = frame.split("\\.");
-            for (String framePart : frameParts) {
-                try {
-                    int frameIndex = Integer.parseInt(framePart);
-                    _webDriver.switchTo().frame(frameIndex);
-                } catch (NumberFormatException ex) {
-                    _webDriver.switchTo().frame(framePart);
-                }
-            }
-        }
-        String html = _webDriver.getPageSource();
-        _document = Jsoup.parse(html);
-    }
+    //    public SeleniumForm bind(final String formId) {
+    //        List<FormBindingException> holder = new ArrayList<>();
+    //        try {
+    //            WebDriverWait webDriverWait = new WebDriverWait(_webDriver, 5, 200);
+    //            return webDriverWait.until(new ExpectedConditionImpl(formId, holder));
+    //        } catch (TimeoutException ex) {
+    //            if (holder.isEmpty()) {
+    //                throw ex;
+    //            }
+    //        }
+    //        throw holder.get(0);
+    //    }
 
-    @Override
-    protected List<Element> createBindingDataInstances(final SeleniumElement parentElement, final String lookup) {
-        Elements elements;
-        if (parentElement == null) {
-            elements = _document.select(lookup);
-        } else {
-            Element initElement = parentElement.getBindingData();
-            elements = initElement.select(lookup);
-        }
-        return elements;
-    }
-
-    @Override
-    protected SeleniumElement createBindedElementInstance(final ElementDefinition elementDefinition, final Element bindingData) {
-        return new SeleniumElement(_webDriver, elementDefinition, bindingData);
-    }
-
-    @Override
-    protected SeleniumFormReference createBindedFormReferenceInstance(final FormReferenceDefinition formReferenceDefinition) {
-        return new SeleniumFormReference(formReferenceDefinition);
-    }
-
-    /**
-     * Bind current html with the form definition.
-     *
-     * @param formId the form ID.
-     *
-     * @return Binded form.
-     */
-    public SeleniumForm bind(final String formId) {
-        List<FormBindingException> holder = new ArrayList<>();
-        try {
-            WebDriverWait webDriverWait = new WebDriverWait(_webDriver, 5, 200);
-            return webDriverWait.until(new ExpectedConditionImpl(formId, holder));
-        } catch (TimeoutException ex) {
-            if (holder.isEmpty()) {
-                throw ex;
-            }
-        }
-        throw holder.get(0);
-    }
-
-    /**
-     * Expected condition for form loading.
-     *
-     * @author Dmitry Shapovalov
-     */
-    private final class ExpectedConditionImpl implements ExpectedCondition<SeleniumForm> {
-
-        private final String _formId;
-
-        private final List<FormBindingException> _holder;
-
-        ExpectedConditionImpl(final String formId, final List<FormBindingException> holder) {
-            super();
-            _formId = formId;
-            _holder = holder;
-        }
-
-        @Override
-        public SeleniumForm apply(final WebDriver webDriver) {
-            try {
-                return doBind(_formId);
-            } catch (FormBindingException ex) {
-                _holder.clear();
-                _holder.add(ex);
-                return null;
-            }
-        }
-
-    }
+    //    private final class ExpectedConditionImpl implements ExpectedCondition<SeleniumForm> {
+    //
+    //        private final String _formId;
+    //
+    //        private final List<FormBindingException> _holder;
+    //
+    //        ExpectedConditionImpl(final String formId, final List<FormBindingException> holder) {
+    //            super();
+    //            _formId = formId;
+    //            _holder = holder;
+    //        }
+    //
+    //        @Override
+    //        public SeleniumForm apply(final WebDriver webDriver) {
+    //            try {
+    //                return doBind(_formId);
+    //            } catch (FormBindingException ex) {
+    //                _holder.clear();
+    //                _holder.add(ex);
+    //                return null;
+    //            }
+    //        }
+    //
+    //    }
 
 }
