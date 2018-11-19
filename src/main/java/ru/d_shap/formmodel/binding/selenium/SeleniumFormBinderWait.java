dd32a63aa7a202cd230e3e28d58e9336a367dff6
Dmitry Shapovalov <shenf@mail.ru>
2018-11-19 14:29:34 +0300

    Класс для циклического выполнения попыток связывания

---
+++ src/main/java/ru/d_shap/formmodel/binding/selenium/SeleniumFormBinderWait.java
@@ -0,0 +1,99 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model selenium binding is a form model binding implementation for Selenium.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model selenium binding.
+//
+// Form model selenium binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model selenium binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+import ru.d_shap.formmodel.Messages;
+import ru.d_shap.formmodel.binding.FormBinder;
+import ru.d_shap.formmodel.binding.FormBindingException;
+import ru.d_shap.formmodel.definition.model.FormDefinition;
+
+/**
+ * Selenium form binder wait.
+ *
+ * @param <T> the generic type of the result.
+ *
+ * @author Dmitry Shapovalov
+ */
+final class SeleniumFormBinderWait<T> {
+
+    private final WebDriver _webDriver;
+
+    private final FormBinder _formBinder;
+
+    private final BindAttempt<T> _bindAttempt;
+
+    private final FormDefinition _formDefinition;
+
+    private final long _timeOutInSeconds;
+
+    private final long _sleepInMillis;
+
+    SeleniumFormBinderWait(final WebDriver webDriver, final FormBinder formBinder, final BindAttempt<T> bindAttempt, final FormDefinition formDefinition, final long timeOutInSeconds, final long sleepInMillis) {
+        super();
+        _webDriver = webDriver;
+        _formBinder = formBinder;
+        _bindAttempt = bindAttempt;
+        _formDefinition = formDefinition;
+        _timeOutInSeconds = timeOutInSeconds;
+        _sleepInMillis = sleepInMillis;
+    }
+
+    T bind() {
+        long endTimeMillis = System.currentTimeMillis() + _timeOutInSeconds * 1000;
+        FormBindingException lastFormBindingException = null;
+        while (true) {
+            try {
+                return _bindAttempt.tryBind(_webDriver, _formBinder);
+            } catch (FormBindingException ex) {
+                lastFormBindingException = ex;
+            }
+            if (System.currentTimeMillis() < endTimeMillis) {
+                try {
+                    Thread.sleep(_sleepInMillis);
+                } catch (InterruptedException ex) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        if (lastFormBindingException == null) {
+            throw new FormBindingException(Messages.Binding.getFormIsNotPresentMessage(_formDefinition));
+        } else {
+            throw lastFormBindingException;
+        }
+    }
+
+    /**
+     * Selenium form binder attempt.
+     *
+     * @param <T> the generic type of the result.
+     *
+     * @author Dmitry Shapovalov
+     */
+    interface BindAttempt<T> {
+
+        T tryBind(WebDriver webDriver, FormBinder formBinder);
+
+    }
+
+}
