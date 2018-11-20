1c216f8fc0c327c03ec9d92de2783de862b97c7e
Dmitry Shapovalov <shenf@mail.ru>
2018-11-20 15:33:26 +0300

    Добавил javadoc для пакета

--- src/main/java/ru/d_shap/formmodel/binding/selenium/package-info.java
+++ src/main/java/ru/d_shap/formmodel/binding/selenium/package-info.java
@@ -18,6 +18,77 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Form-model implementation for Selenium.
+ * <p>
+ * Form model selenium binding is a form model binding implementation for Selenium.
+ * </p>
+ * <p>
+ * This is the Page Object pattern implementation.
+ * </p>
+ * <p>
+ * For example, suppose the following source page:
+ * </p>
+ * <pre>
+ * &lt;html&gt;
+ *     &lt;head&gt;
+ *         &lt;title&gt;Login page&lt;/title&gt;
+ *     &lt;/head&gt;
+ *     &lt;body&gt;
+ *         &lt;form action="http://example.com"&gt;
+ *             &lt;div&gt;
+ *                 &lt;span&gt;User name:&lt;/span&gt;
+ *                 &lt;input name="username" type="text"/&gt;
+ *             &lt;/div&gt;
+ *             &lt;div&gt;
+ *                 &lt;span&gt;Password:&lt;/span&gt;
+ *                 &lt;input name="password" type="password"/&gt;
+ *             &lt;/div&gt;
+ *             &lt;div&gt;
+ *                 &lt;input type="submit" value="Login"/&gt;
+ *             &lt;/div&gt;
+ *         &lt;/form&gt;
+ *     &lt;/body&gt;
+ * &lt;/html&gt;
+ * </pre>
+ * <p>
+ * This is a simple login form with inputs for user name and password and submit button.
+ * </p>
+ * <p>
+ * The model for this login form is the following:
+ * </p>
+ * <pre>
+ * &lt;?xml version="1.0"?&gt;
+ * &lt;ns1:form id="login" xmlns:ns1="http://d-shap.ru/schema/form-model/1.0"&gt;
+ *     &lt;ns1:element id="username" lookup="input[name='username']"/&gt;
+ *     &lt;ns1:element id="userpass" lookup="input[name='password']"/&gt;
+ *     &lt;ns1:element id="submit" lookup="input[type='submit']"/&gt;
+ * &lt;/ns1:form&gt;
+ * </pre>
+ * <p>
+ * The following code implements the binding and interaction with the binded model:
+ * </p>
+ * <pre>{@code
+ * // Load form definitions
+ * FormDefinitions formDefinitions = new FormDefinitions();
+ * File file = new File("file with the form definition");
+ * FormDefinitionsLoader formDefinitionsLoader = new FormXmlDefinitionsFileLoader(file);
+ * formDefinitionsLoader.load(formDefinitions);
+ * SeleniumFormBinder formBinder = new SeleniumFormBinder(formDefinitions);
+ *
+ * // Create WebDriver
+ * WebDriver webDriver = ...
+ *
+ * // Bind the source page
+ * String url = ...
+ * Document document = seleniumFormBinder.bind(webDriver, "login");
+ *
+ * // Use the binded form to login
+ * SeleniumBindedElement bindedElement;
+ * bindedElement = seleniumFormBinder.getBindedElementWithId(document, "username");
+ * bindedElement.sendKeys("user");
+ * bindedElement = seleniumFormBinder.getBindedElementWithId(document, "userpass");
+ * bindedElement.sendKeys("password");
+ * bindedElement = seleniumFormBinder.getBindedElementWithId(document, "submit");
+ * bindedElement.click();
+ * }</pre>
  */
 package ru.d_shap.formmodel.binding.selenium;
